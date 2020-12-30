ea53ef791f0cf56ade4260d6a9d50ea37241b30b
KhripunovS <72104467+KhripunovS@users.noreply.github.com>
2020-12-30 17:10:03 +0300

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="assets/css/main.css">
+	<title>Авторизация и регистрация</title>
+</head>
+<body>
+<div class="intro"> 
+	<form action="" method="">
+		<label>ФИО</label>
+		<input type="text" placeholder="Введите свое полное имя">
+		<label>Логин</label>
+		<input type="text" placeholder="Введите свой логин">
+		<label>Почта</label>
+		<input type="email" placeholder="Введите адрес своей почты">
+		<label>Изображение профиля</label>
+		<input type="file">
+		<label>Пароль</label>
+		<input type="password" placeholder="Введите пароль">
+		<label>Подтверждение пароля</label>
+		<input type="password" placeholder="Введите пароль еще раз">
+		<button>Войти</button>
+		<p>
+			У вас уже есть аккаунт? - <a href="login.php">Авторизуйтесь</a>!
+		</p>
+	</form>
+	</div>
+</body>
+</html>
